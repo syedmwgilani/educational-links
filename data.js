794310388065c82882de8dc61ba93b497addb6a7
syedmwgilani <syedmwgilani@gmail.com>
2022-02-10 17:58:59 -0500

    Added all the files

---
+++ data.js
@@ -0,0 +1,56 @@
+/*
+SAMPLE OF CATEGORY OBJECT
+
+{
+  name: 'New Category',
+  links: [
+    {
+      text: 'Google',
+      url: 'https://www.google.com',
+      title: 'Google'
+    },
+    {
+      text: 'Yahoo',
+      url: 'https://www.yahoo.com',
+      title: 'Yahoo'
+    }
+  ]
+}
+
+*/
+
+let data = [
+  {
+    name: 'New Category',
+    links: [
+      {
+        text: 'Google',
+        url: 'https://www.google.com',
+        title: 'Google'
+      },
+      {
+        text: 'Yahoo',
+        url: 'https://www.yahoo.com',
+        title: 'Yahoo'
+      }
+    ]
+  }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
